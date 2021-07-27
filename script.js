cf594e829f35cec1e54b8b24ca24d997912655f4
Fidelis Raymond <arjefidelisraymond@gmail.com>
2021-07-27 22:32:42 +0700

    Assignment 1 - Values and Variables, fundamental part 1

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let js = "amazing";
-// if (js === "amazing") alert("JS is AWSOME!");
+// let js = "amazing";
+// // if (js === "amazing") alert("JS is AWSOME!");
 
-console.log(26+27*2);
+// console.log(26+27*2);
+
+// Assignment 1 - Values and Variable
+let country = "Indonesia";
+let continent = "Central Java";
+let population = 1000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
