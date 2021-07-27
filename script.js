b4db4b98811665b6027bc9e4016432629ef186d2
Fidelis Raymond <arjefidelisraymond@gmail.com>
2021-07-27 23:16:37 +0700

    Data Types and Variable declaration, fundamental part 1

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // console.log(26+27*2);
 
 // Assignment 1 - Values and Variable
+/*
 let country = "Indonesia";
 let continent = "Central Java";
 let population = 1000000;
@@ -11,3 +12,14 @@ let population = 1000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+*/
+console.log(a);
+var a = 10;
+console.log(a);
+firstName = "Fidelis Raymond";
+console.log(firstName);
+
+for(let i = 0; i < 3;i++){
+    console.log(i);
+}
+console.log(i);
