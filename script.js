efec39400d80997ee54a414702f93cfc0f9c67d3
Fidelis Raymond <arjefidelisraymond@gmail.com>
2021-08-10 18:07:38 +0700

    Truthy and Falsy Value, fundamental part 1

--- script.js
+++ script.js
@@ -89,13 +89,36 @@ console.log(kata3);*/
     This includes conversion from Number to String, String to Number, Boolean to Number etc. when different types of operators are applied to the values.
 */
 
-const teks = '5';
+/*const teks = '5';
 const angka = Number(teks); // Mengubah teks '5' (tipe data String) menjadi sebuah angka 5 (tipe data Number)
+*/
 
-console.log(angka, typeof angka);
+/* console.log(angka, typeof angka);
 console.log(teks , typeof teks);
 
 console.log(('5' + '5')/ 55); // Hasilnya → 1 (tipe datanya Number)
 console.log(('5' + '5' - '20') + 5); // Hasilnya → 40 (tipe datanya Number)
 
-console.log(`saya suka nonton bioskop di` + ` ` + `21` - `11`);
+console.log(`saya suka nonton bioskop di` + ` ` + `21` - `11`); */
+
+///////////////////////////////////////////
+// Truthy and Falsy Value
+
+/* Note : 
+    Falsy Value : 0, '', undefined, NaN, null
+*/
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('False'));
+console.log(Boolean({}));
+
+// const money = Number('Fidelis');
+const money = 10;
+console.log(`Money : ${money}`);
+if(money) console.log(`Don't spend it all 😊`);
+else console.log(`You should get a job!!`);
+
+let height = 10;
+if(height) console.log(`YAY! height is defined`);
+else console.log(`height is UNDEFINED`);
