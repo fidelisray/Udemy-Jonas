b8c538f1bb181f3c18bb7724394199037ce6035a
Fidelis Raymond <arjefidelisraymond@gmail.com>
2021-08-10 13:54:08 +0700

    Mengerjakan coding challenge ke 2, fundamental part 1

--- codingChallenge.js
+++ codingChallenge.js
@@ -42,3 +42,11 @@ console.log("Mark's BMI = " + markBMI);
 console.log("John's BMI = " + johnBMI);
 console.log("Is Mark's BMI higher than John's ? " + markHigherBMI);
 
+/////////////////////////////////////
+// Coding Challenge #2
+
+if(markBMI > johnBMI) console.log(`Mark's BMI is higher than John's`);
+else console.log(`John's BMI is higher than Mark's`);
+
+if(markBMI > johnBMI) console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
+else console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
