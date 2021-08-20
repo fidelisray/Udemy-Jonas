d01394a859597e0e6009f7c3adc8aa19a1130eb4
Fidelis Raymond <arjefidelisraymond@gmail.com>
2021-08-20 18:04:35 +0700

    Switch statement, fundamental part 1

--- script.js
+++ script.js
@@ -108,17 +108,44 @@ console.log(`saya suka nonton bioskop di` + ` ` + `21` - `11`); */
     Falsy Value : 0, '', undefined, NaN, null
 */
 
-console.log(Boolean(0));
+/*console.log(Boolean(0));
 console.log(Boolean(undefined));
 console.log(Boolean('False'));
-console.log(Boolean({}));
+console.log(Boolean({}));*/
 
 // const money = Number('Fidelis');
-const money = 10;
+/*const money = 10;
 console.log(`Money : ${money}`);
 if(money) console.log(`Don't spend it all 😊`);
 else console.log(`You should get a job!!`);
 
 let height = 10;
 if(height) console.log(`YAY! height is defined`);
-else console.log(`height is UNDEFINED`);
+else console.log(`height is UNDEFINED`);*/
+
+///////////////////////////////////////////
+// Switch Statements
+
+let day = 'wednesday';
+
+switch (day) {
+    case 'monday':
+        console.log('Belajar javascript (udemy)');
+        break;
+    case 'tuesday':
+        console.log('Belajar laravel (Web Programming UNPAS)');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Mencari dan membaca jurnal');
+        break;
+    case 'friday':
+        console.log('Belajar javascript (Youtube / WPU)');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Istirahat');
+        break;
+    default:
+        console.log('Hari yang anda masukkan TIDAK VALID!!');
+}
